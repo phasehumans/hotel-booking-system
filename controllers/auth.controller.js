8ffc8b355337894ecb35dc3e4447bf68fabe1d73
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-01-28 15:05:09 +0530

    login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import { pool } from "../config/db.js"
-import { signupSchema } from "../utils/validation.js"
+import { loginSchema, signupSchema } from "../utils/validation.js"
 import bcrypt from "bcrypt"
 import { randomUUID } from "crypto";
+import jwt from "jsonwebtoken"
 
 
 export const signup = async(req, res) => {
@@ -63,5 +64,76 @@ export const signup = async(req, res) => {
 }
 
 export const login = async(req, res) => {
+    const parseData = loginSchema.safeParse(req.body)
 
+    if(!parseData.success){
+        return res.status(400).json({
+            success: false,
+            message: "invalid inputs",
+            data: null,
+            error: parseData.error.flatten()
+        })
+    }
+
+    const {email, password} = parseData.data
+
+    try {
+        const query = `
+            SELECT id, name, email, password, role
+            FROM users
+            WHERE email= $1
+        `
+    
+        const values = [
+            email.toLowerCase()
+        ]
+    
+        const {rows} = await pool.query(query, values)
+    
+        if(rows.length === 0){
+            return res.status(401).json({
+                success: false,
+                message: "invalid email or password",
+                data: null,
+            })
+        }
+    
+        const user = rows[0]
+    
+        const isPassworsMatch = await bcrypt.compare(password, user.password)
+    
+        if(!isPassworsMatch){
+            return res.status(401).json({
+              success: false,
+              message: "invalid email or password",
+              data: null,
+            });
+        }
+    
+        const token = jwt.sign({
+            id: user.id,
+            role: user.role
+        }, process.env.JWT_SECRET)
+    
+        return res.status(200).json({
+            success: true,
+            message: "login successful",
+            data: {
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role
+                },
+                token
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "internal server error",
+            data: null,
+            error: error.message
+        })
+    }
 }
--- utils/validation.js
+++ utils/validation.js
@@ -9,7 +9,8 @@ export const signupSchema = z.object({
 })
 
 export const loginSchema = z.object({
-
+    email: z.string().email(),
+    password: z.string().min(3).max(20)
 })
 
 export const createHotelSchema = z.object({
